938dc2d7669eaa485b1cd072d3fe3e15de68c80b
HarvyDev <franciscolbantunes@gmail.com>
2024-01-24 19:52:00 +0000

    initial config

---
+++ lua/vim-options.lua
@@ -0,0 +1,12 @@
+vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set expandtab")
+vim.cmd("set nowrap")
+vim.cmd("set scrolloff=8")
+vim.cmd("set incsearch")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+vim.cmd("set hlsearch")
+vim.cmd("set history=1000")
+vim.g.mapleader = " "

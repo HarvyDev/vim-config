251b191636a6c7d1c5bc1b2878dfe4d2b28aa501
HarvyDev <franciscolbantunes@gmail.com>
2024-03-01 23:02:49 +0000

    Obsidian added

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "cpp", "lua", "rust",
-                "jsdoc", "bash",
+                "jsdoc", "bash", "markdown", "markdown_inline",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -15,3 +15,16 @@ vim.g.mapleader = " "
 
 -- Cursor 
 vim.opt.guicursor = ""
+
+vim.opt.conceallevel = 2
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+-- Obsidian
+
+vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open obsidian app" })
+vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "New obsidian note" })
+vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show obsidian links" })
+vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show obsidian backlinks" })
+vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick switch obsidian note" })
+vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search obsidian" })

f48643b3d70248549721d92c9c3bc3c6c36275d9
HarvyDev <franciscolbantunes@gmail.com>
2024-03-10 13:44:42 +0000

    added lsp keybinds

--- lua/remap.lua
+++ lua/remap.lua
@@ -5,3 +5,13 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+
+-- Obsidian 
+vim.keymap.set("n", "<leader>oo", "<cmd>ObsidianOpen<CR>", { desc = "Open obsidian app" })
+vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "New obsidian note" })
+vim.keymap.set("n", "<leader>ol", "<cmd>ObsidianLinks<CR>", { desc = "Show obsidian links" })
+vim.keymap.set("n", "<leader>ob", "<cmd>ObsidianBacklinks<CR>", { desc = "Show obsidian backlinks" })
+vim.keymap.set("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick switch obsidian note" })
+vim.keymap.set("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search obsidian" })
+
